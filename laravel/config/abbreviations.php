e76ceb8940f051b102c4ae28ea78ca9f401999a4
Carlos Suescún <csueskun@gmail.com>
2025-03-17 23:33:04 +0100

    Add abbreviations entries

--- laravel/config/abbreviations.php
+++ laravel/config/abbreviations.php
@@ -5,4 +5,7 @@ return [
     'CLTVA' => 'Certificado de libertad y tradición de un vehículo automotor',
     'DPVA' => 'Duplicado de placas de un vehículo automotor',
     'RLC' => 'Renovación de licencia de conducción',
+    'DOC:ID' => 'Documento de Identidad',
+    'DOC:PROPIEDAD' => 'Tarjeta de propiedad del vehículo automotor',
+    'DOC:FUN' => 'Formato FUN',
 ];
