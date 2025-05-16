024c01dd35a01fa41c46060489c326a6adf7502d
Carlos Monsalve <csueskun@gmail.com>
2025-05-16 13:00:14 -0400

    Update migrations

---
+++ laravel/database/factories/TramiteItemFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comentario>
+ */
+class TramiteItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->word(),
+            'descripcion' => $this->faker->paragraph(),
+            'precio' => $this->faker->randomFloat(2, 0, 10000),
+            'tramite_id' => \App\Models\Tramite::factory(),
+        ];
+    }
+}
