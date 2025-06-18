46ad4813144e1c3e9f2f664ce88936bdf57f5fbf
Carlos Monsalve <csueskun@gmail.com>
2025-06-18 11:52:29 -0400

    Update file input component

--- images/php/laravel/resources/views/components/gov-file-input.blade.php
+++ images/php/laravel/resources/views/components/gov-file-input.blade.php
@@ -12,7 +12,7 @@
                     seleccionado</span>
             </label>
             <span class="text-validation-carga-de-archivo-govco">
-                Tipo de archivo: {{ $tipo }}. Peso máximo: {{ $max }} MB
+                Tipo de archivo: <strong>{{ $type }}</strong>. Peso máximo: {{ $max }} MB
             </span>
         </div>
         <div class="load-button-carga-de-archivo-govco" style="display: none;">
