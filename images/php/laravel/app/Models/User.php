00191a4daf89adf74e827906391a80fecbce7b3b
Carlos Monsalve <csueskun@gmail.com>
2025-06-18 11:33:42 -0400

    Fix password validation regex to allow dot character

--- images/php/laravel/app/Models/User.php
+++ images/php/laravel/app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable
                 'string',
                 'min:8',
                 'confirmed',
-                'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
+                'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]+$/',
             ],
         ];
     }
