3c5286eebb15498b646bfdabe2724a133c4fe5be
Carlos Monsalve <csueskun@gmail.com>
2025-06-24 00:10:49 -0400

    Add password recovery mail

---
+++ images/php/laravel/app/Mail/ResetPassword.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Attachment;
+
+class ResetPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $usuario;
+    private $token;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($usuario, $token)
+    {
+        $this->usuario = $usuario;
+        $this->token = $token;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Recuperación de Contraseña - Secretaría de Tránsito, sede operativa El Zulia',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        $url = url("/reset-password?token={$this->token}&email=" . urlencode($this->usuario->email));
+        return new Content(
+            view: 'emails.password-reset',
+            with: [
+                'nombres' => $this->usuario->nombre_completo,
+                'url' => $url,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ images/php/laravel/resources/views/emails/password-reset.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Estimado <i>{{ $nombres }}</i>,</p>
+    <p>
+        Para crear una nueva contraseña, por favor haga clic en el siguiente enlace:
+        <a href="{{ $url }}">Crear nueva contraseña</a>.
+    </p>
+    <p>
+        Si tiene alguna consulta adicional o requiere asistencia, 
+        no dude en comunicarse con nosotros a través de 
+        <a href="mailto:transito.sistemas@nortedesantander.gov.co">transito.sistemas@nortedesantander.gov.co</a>
+    </p>
+    <p>
+        Atentamente,<br>
+        Secretaría de Tránsito, sede operativa El Zulia<br>
+    </p>
+</body>
+</html>
