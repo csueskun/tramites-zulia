246eace630ea5cd069e1e63ff89d191107b76556
Carlos Monsalve <csueskun@gmail.com>
2025-07-25 14:02:58 -0400

    Refactor email content in cupl.blade.php for improved clarity and emphasis on payment deadline

--- images/php/laravel/resources/views/emails/cupl.blade.php
+++ images/php/laravel/resources/views/emails/cupl.blade.php
@@ -10,18 +10,20 @@
 <body>
     <p>Estimado <i>{{ $nombres }}</i>,</p>
     <p>
-        Le informamos que el recibo de pago de TNS fue enviado a su correo electrónico, 
-        y <strong><span style="color: red;">
-        TIENE PLAZO EL DIA DE HOY HASTA LAS 11:59 PM PARA REALIZAR EL CORRESPONDIENTE PAGO
-        </span></strong>.
+        Le informamos que el recibo de pago de TNS fue enviado a su correo electrónico, y 
+        <strong><span style="color: red;">
+            TIENE PLAZO EL DIA DE HOY HASTA LAS 11:59 PM PARA REALIZAR EL CORRESPONDIENTE PAGO
+        </span></strong>
     </p>
     <p>
         De igual forma, se adjunta el Recibo de pago correspondiente al derecho a 
         tramite del RUNT (CUPL) para realizar el pago correspondiente.
     </p>
     <p>
-        Recuerde que de no realizarse el pago en el plazo indicado, será rechazado 
-        el trámite y deberá volver a realizar una nueva solicitud.
+        <strong><span style="color: red;">
+            Recuerde que de no realizarse el pago en el plazo indicado, será rechazado 
+            el trámite y deberá volver a realizar una nueva solicitud.
+        </span></strong>
     </p>
     <p>
         Si tiene alguna consulta adicional o requiere asistencia, 
